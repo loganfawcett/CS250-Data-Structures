f43dd8ec63ffb70dedf745a8719091d2f7b52bc1
svc-student <svc-student@student.directory.jccc>
2017-01-25 19:14:57 -0600

    Updated linear search to include last result

--- Lab 2/Stepping/Stepping/lab2.cpp
+++ Lab 2/Stepping/Stepping/lab2.cpp
@@ -31,7 +31,7 @@ int LinearSearch( const vector<string>& list, string findMe, int& cycleCounter )
         
         cycleCounter = 0;
                 
-		for (int i = 0; i < list.size() - 1; i++)
+		for (int i = 0; i <= list.size() - 1; i++)
 		{
 			cycleCounter++;
 			if (list[i] == findMe)
