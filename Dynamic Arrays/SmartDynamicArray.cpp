ed5c7a7792fd856416c47f2652916963a9c6e821
svc-student <svc-student@student.directory.jccc>
2017-02-08 20:22:17 -0600

    Finished Dynamic Arrays

--- Dynamic Arrays/SmartDynamicArray.cpp
+++ Dynamic Arrays/SmartDynamicArray.cpp
@@ -2,87 +2,181 @@
 
 #include "cuTEST/Menu.hpp"
 
-SmartDynamicArray::SmartDynamicArray()
+SmartDynamicArray::SmartDynamicArray() : m_itemCount(0), m_arraySize(0), m_data(nullptr)
 {
 }
 
 SmartDynamicArray::~SmartDynamicArray()
 {
+	Free();
 }
 
-bool SmartDynamicArray::Push( const string& newItem )
+bool SmartDynamicArray::Push(const string& newItem)
 {
-	return false; // temp
+	if (m_itemCount == m_arraySize)
+	{
+		Resize();
+	}
+
+	m_data[m_itemCount] = newItem;
+	m_itemCount++;
+	return true;
 }
 
 
-bool SmartDynamicArray::Insert( int index, const string& newItem )
+bool SmartDynamicArray::Insert(int index, const string& newItem)
 {
-	return false; // temp
+	if (m_itemCount == m_arraySize)
+	{
+		Resize();
+	}
+	if (index < 0 || index > this->Size())
+	{
+		return false;
+	}
+
+	int i = m_arraySize - 1;
+	while (i > index)
+	{
+		m_data[i] = m_data[i - 1];
+		i--;
+	}
+	m_data[index] = newItem;
+	m_itemCount++;
+	return true;
 }
 
 
-bool SmartDynamicArray::Extend( const SmartDynamicArray& other )
+bool SmartDynamicArray::Extend(const SmartDynamicArray& other)
 {
-	return false; // temp
+	Resize(this->Size() + other.Size());
+
+	for (int j = 0; j < other.Size(); j++)
+	{
+		this->Push(other.Get(j));
+	}
+	return true;
 }
 
 
 bool SmartDynamicArray::Pop()
 {
-	return false; // temp
+	if (m_itemCount == 0)
+	{
+		return false;
+	}
+	else
+	{
+		m_itemCount--;
+		return true;
+	}
 }
 
 
-bool SmartDynamicArray::Remove( int index )
+bool SmartDynamicArray::Remove(int index)
 {
-	return false; // temp
+	if (index >= m_itemCount || index < 0)
+	{
+		return false;
+	}
+	else
+	{
+		int i = index;
+		while (i < m_arraySize - 2)
+		{
+			m_data[i] = m_data[i + 1];
+			i++;
+		}
+		m_data[i] = "";
+		m_itemCount--;
+		return true;
+	}
 }
 
 
-string SmartDynamicArray::Get( int index ) const
+string SmartDynamicArray::Get(int index) const
 {
-	return ""; // temp
+	if (index >= m_arraySize || index < 0)
+	{
+		return "";
+	}
+	else
+	{
+		return m_data[index]; // temp
+	}
 }
 
 
 int SmartDynamicArray::Size() const
 {
-	return -1; // temp
+	return m_itemCount; // temp
 }
 
 int SmartDynamicArray::GetMaxSize() const
 {
-    return -1; // temp
+	return -1; // temp
 }
 
 bool SmartDynamicArray::IsFull() const
 {
-	return false; // temp
+	if (this->Size() == m_arraySize)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 bool SmartDynamicArray::IsEmpty() const
 {
-	return false; // temp
+	if (this->Size() == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-string SmartDynamicArray::operator[]( int index )
+
+string SmartDynamicArray::operator[](int index)
 {
-	return ""; // temp
+	return this->Get(index);
 }
 
-SmartDynamicArray& SmartDynamicArray::operator=( const SmartDynamicArray& other )
+SmartDynamicArray& SmartDynamicArray::operator=(const SmartDynamicArray& other)
 {
-    return *this; // temp
+	Free();
+	Resize(other.Size());
+	for (int i = 0; i < m_arraySize; i++)
+	{
+		this->m_data[i] = other.Get(i);
+	}
+	return *this; // placeholder
 }
 
-bool SmartDynamicArray::operator==( const SmartDynamicArray& other )
+bool SmartDynamicArray::operator==(const SmartDynamicArray& other)
 {
-	return false; // temp
+	if (this->Size() != other.Size())
+	{
+		return false;
+	}
+
+	for (int i = 0; i < m_arraySize; i++)
+	{
+		if (this->Get(i) != other.Get(i))
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
 
-
-bool SmartDynamicArray::operator!=( const SmartDynamicArray& other )
+bool SmartDynamicArray::operator!=(const SmartDynamicArray& other)
 {
 	return false; // temp
 }
@@ -93,11 +187,21 @@ void SmartDynamicArray::Free()
 
 void SmartDynamicArray::Resize()
 {
+	Resize(m_arraySize+10);
 }
 
 
-void SmartDynamicArray::Resize( int newSize )
+void SmartDynamicArray::Resize(int newSize)
 {
+	string* newArray = new string[newSize];
+
+	for (int i = 0; i < this->Size(); i++)
+	{
+		newArray[i] = this->m_data[i];
+	}
+	Free();
+	m_data = newArray;
+	m_arraySize = newSize;
 }
 
 
