85fdfc36fc068863c75ba7a8859932feff477efc
svc-student <svc-student@student.directory.jccc>
2017-02-15 20:09:46 -0600

    Finished kinked lists

--- Linked List/LinkedList.hpp
+++ Linked List/LinkedList.hpp
@@ -38,30 +38,157 @@ class LinkedList
 
     void Push( const T& newItem )
     {
+		Node<T>* newNode = new Node<T>;
+		newNode->data = newItem;
+		
+		if (m_ptrFirst == nullptr)
+		{
+			//Empty list
+			m_ptrFirst = newNode;
+			m_ptrLast = newNode;
+		}
+		else
+		{
+			// Not empty
+			newNode->ptrPrev = m_ptrLast;
+			m_ptrLast->ptrNext = newNode;
+			m_ptrLast = newNode;
+		}
+
+		m_itemCount++;
     }
 
     bool Insert( int index, const T& newItem )
     {
-        return false; // temp
+		//	Error Checking.
+		if (index < 0 || index > m_itemCount)
+		{
+			return false; //	Return new T item.
+		}
+		else if (index == m_itemCount)
+		{
+			this->Push(newItem);
+			return true;
+		}
+		else
+		{
+			//	Create traversal pointer
+			Node<T>* ptrCurrent = m_ptrFirst;
+			int counter = 0;
+			//	Traverse list to find index.
+			while (counter < index)
+			{
+				ptrCurrent = ptrCurrent->ptrNext;
+				counter++;
+			}
+
+			Node<T>* ptrNew = new Node<T>;
+			ptrNew->data = newItem;
+
+			ptrNew->ptrNext = ptrCurrent;
+			ptrNew->ptrPrev = ptrCurrent->ptrPrev;
+			ptrNew->ptrPrev->ptrNext = ptrNew;
+			ptrCurrent->ptrPrev = ptrNew;
+
+			m_itemCount++;
+			return true;
+		}
+
+
+
     }
 
     void Extend( const LinkedList& other )
     {
+		Node<T>* ptrCurrent = other.m_ptrFirst;
+		int count = 0;
+
+		while (count < other.Size())
+		{
+			this->Push(other.Get(count));
+			count++;
+		}
     }
 
     bool Pop()
     {
-        return false; // temp
+		if (IsEmpty())
+		{
+			return false;
+		}
+
+		if (Size() > 1)
+		{
+			m_ptrLast = m_ptrLast->ptrPrev;
+			delete m_ptrLast->ptrNext;
+			m_ptrLast->ptrNext = nullptr;
+			m_itemCount--;
+			return true;
+		}
+		else
+		{
+			delete m_ptrFirst;
+			m_ptrFirst = m_ptrLast = nullptr;
+			m_itemCount--;
+			return true;
+		}
     }
 
     bool Remove( int index )
     {
-        return false; // temp
+		//	Error Checking.
+		if (index < 0 || index >= m_itemCount)
+		{
+			return false; //	Return new T item.
+		}
+		else if (m_itemCount == 1)
+		{
+			return this->Pop();
+		}
+		else if (index == 0)
+		{
+			m_ptrFirst = m_ptrFirst->ptrNext;
+			delete m_ptrFirst->ptrPrev;
+			m_itemCount--;
+			return true;
+		}
+		else
+		{
+			//	Create traversal pointer
+			Node<T>* ptrCurrent = m_ptrFirst;
+			int counter = 0;
+			//	Traverse list to find index.
+			while (counter < index)
+			{
+				ptrCurrent = ptrCurrent->ptrNext;
+				counter++;
+			}
+
+			ptrCurrent->ptrPrev->ptrNext = ptrCurrent->ptrNext;
+			ptrCurrent->ptrNext->ptrPrev = ptrCurrent->ptrPrev;
+			delete ptrCurrent;
+			m_itemCount--;
+			return true;
+		}
     }
 
     T Get( int index ) const
     {
-        return T(); // temp
+		//	Error Checking.
+		if (index < 0 || index >= m_itemCount)
+		{
+			return T(); //	Return new T item.
+		}
+		Node<T>* ptrCurrent = m_ptrFirst;
+		int counter = 0;
+		//	Traverse list to find index.
+		while (counter < index)
+		{
+			ptrCurrent = ptrCurrent->ptrNext;
+			counter++;
+		}
+		//	Return data at index.
+		return ptrCurrent->data;
     }
 
     int Size() const // done
@@ -81,12 +208,41 @@ class LinkedList
 
     LinkedList& operator=( const LinkedList& other )
     {
-        return (*this); // temp
+		Free();
+		Node<T>* ptrCurrent = other.m_ptrFirst;
+		int count = 0;
+
+		while (count < other.Size())
+		{
+			this->Push(other.Get(count));
+			count++;
+		}
+		return *this;
     }
 
     bool operator==( const LinkedList& other )
     {
-        return false; // temp
+		if (this->Size() != other.Size())
+		{
+			return false;
+		}
+
+		//	Create traversal pointer
+		Node<T>* ptrCurrent = this->m_ptrFirst;
+		Node<T>* ptrOther = other.m_ptrFirst;
+		int counter = 0;
+		//	Traverse list to find index.
+		while (counter < this->Size())
+		{
+			if (ptrCurrent->data != ptrOther->data)
+			{
+				return false;
+			}
+			ptrCurrent = ptrCurrent->ptrNext;
+			ptrOther = ptrOther->ptrNext;
+			counter++;
+		}
+		return true;
     }
 
     bool operator!=( const LinkedList& other )
@@ -105,7 +261,7 @@ class LinkedList
 
     Node<T>* m_ptrFirst;
     Node<T>* m_ptrLast;
-    int m_itemCount;
+    int m_itemCount = 0;
 };
 
 #endif
